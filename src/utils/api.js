c48f8f1d611340321d451725b519e10417440a41
arun <arun@aruns-MBP.bbrouter>
2024-12-14 01:50:36 +0530

    url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import { useLoader } from '../contexts/LoaderContext'; // Ensure proper path
-import React from 'react';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -8,11 +6,11 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
-const setupInterceptors = (setIsLoading) => {
+// Set up interceptors globally
+const setupInterceptors = () => {
   // Request Interceptor
   api.interceptors.request.use(
     (config) => {
-      setIsLoading(true);
       const token = localStorage.getItem('token');
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -20,32 +18,20 @@ const setupInterceptors = (setIsLoading) => {
       return config;
     },
     (error) => {
-      setIsLoading(false);
       return Promise.reject(error);
     }
   );
 
   // Response Interceptor
   api.interceptors.response.use(
-    (response) => {
-      setIsLoading(false);
-      return response;
-    },
+    (response) => response,
     (error) => {
-      setIsLoading(false);
       return Promise.reject(error);
     }
   );
 };
 
-export const ApiProvider = ({ children }) => {
-  const { setIsLoading } = useLoader();
-
-  React.useEffect(() => {
-    setupInterceptors(setIsLoading);
-  }, [setIsLoading]);
-
-  return children;
-};
+// Run setup on initialization (ensure interceptors are added only once)
+setupInterceptors();
 
 export default api;
